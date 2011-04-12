aa7eb9640acf3e4556460a2a7143f6fbaa09c2ba
Paul Sturgess <paulsturgess@gmail.com>
2011-04-12 23:39:18 +0100

    Add :on_or_after option

--- date_validator.rb
+++ date_validator.rb
@@ -4,7 +4,11 @@ class DateValidator < ActiveModel::EachValidator
     return unless value
     if options[:after]
       after_value = options[:after][:value].call(object)
-      object.errors[attribute] << "#{options[:after][:message]}" if after_value && value < after_value
+      object.errors[attribute] << "#{options[:after][:message]}" if after_value && (value <= after_value)
+    end
+    if options[:on_or_after]
+      on_or_after_value = options[:on_or_after][:value].call(object)
+      object.errors[attribute] << "#{options[:on_or_after][:message]}" if on_or_after_value && (value < on_or_after_value)
     end
   end
   
